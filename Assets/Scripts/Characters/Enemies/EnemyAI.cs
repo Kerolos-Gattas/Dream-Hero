745ecf350b2aee89ff94a342c2a527c27f67da47
Kerolos-Gattas <kerolos_gattas@hotmail.com>
2021-04-11 00:11:45 -0400

    Experimenting with path finding

--- Assets/Scripts/Characters/Enemies/EnemyAI.cs
+++ Assets/Scripts/Characters/Enemies/EnemyAI.cs
@@ -3,10 +3,13 @@ using Pathfinding;
 
 public class EnemyAI : MonoBehaviour
 {
+    //TODO create state machine
+    //TODO enemy ai should be able to jump through platforms
     public Transform target;
     public float speed = 80f;
     public float nextWayPointDistance = 3f;
     public Transform gfx;
+    public float gravityForceScale = 5f;
 
     private Path path;
     private int currentWayPoint = 0;
@@ -39,6 +42,7 @@ public class EnemyAI : MonoBehaviour
 
     private void FixedUpdate()
     {
+        IgnorePlatformCollisions();
         if (path == null)
         {
             return;
@@ -54,8 +58,9 @@ public class EnemyAI : MonoBehaviour
             reachedEndOfPath = false;
         }
 
-        Vector2 dir = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
+        Vector2 dir = ((Vector2)path.vectorPath[currentWayPoint] - rb.position);
         Vector2 force = dir * speed * Time.deltaTime;
+        force = new Vector2(force.x, force.y * gravityForceScale);
         rb.AddForce(force);
 
         float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
@@ -72,6 +77,18 @@ public class EnemyAI : MonoBehaviour
         else if (force.x <= -0.01f)
         {
             gfx.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+
+    private void IgnorePlatformCollisions()
+    {
+        if (this.rb.velocity.y > 0)
+        {
+            Physics2D.IgnoreLayerCollision(10, 12, true);
+        }
+        else
+        {
+            Physics2D.IgnoreLayerCollision(10, 12, false);
         }
     }
 }
